2ac545d44b8733f6fdce86a97367888fdcbbfb70
Julio Romero <105522237+Jumicode@users.noreply.github.com>
2023-09-13 20:32:01 +0000

    API call

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,24 @@
-function Home(){
+async function fetchUsers(){
+    const res = await  fetch('https://reqres.in/api/users?page=2');
+    const data = await res.json();
+    return data.data;
+}
+
+async function Home(){
+
+    const users = await fetchUsers();
     return(
-        <h1>Home</h1>
+        <ul className="">
+            {users.map(user => (
+                <li key={user.id}>
+                <div>
+          <h5>{user.id} {user.first_name} {user.last_name}</h5>
+          <p>email: {user.email}</p>
+
+                </div>
+                </li>
+            ))}
+        </ul>
     )
 }
 
